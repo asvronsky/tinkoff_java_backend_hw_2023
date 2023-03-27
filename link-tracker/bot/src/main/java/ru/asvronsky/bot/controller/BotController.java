e4eb499f4c07a19981888643500f968ade6e8fa7
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 21:29:16 +0300

    Update BotController

--- link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
+++ link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
@@ -4,21 +4,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import ru.asvronsky.bot.dto.UpdateLinksResponse;
+import ru.asvronsky.bot.services.BotService;
 
 @RestController
 @RequestMapping("/updates")
+@RequiredArgsConstructor
 public class BotController {
+    private final BotService botService;
     
     @PostMapping
     public UpdateLinksResponse updateLinks() {
-
-        return new UpdateLinksResponse(
-            0, 
-            "google.com", 
-            "description", 
-            null
-        );
+        return botService.updateLinks();
     }
 
 }
