a149e8078221c8c235c64e8ff9c765a36f2bcf97
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 23:01:59 +0300

    Update BotController docs

--- link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
+++ link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -20,6 +21,7 @@ import ru.asvronsky.bot.services.BotService;
 public class BotController {
     private final BotService botService;
     
+    @Operation(summary = "Request update")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Updated"),
         @ApiResponse(responseCode = "400", description = "Invalid request format",
