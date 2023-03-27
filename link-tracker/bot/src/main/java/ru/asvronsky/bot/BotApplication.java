cf722d411462c857eb241c3da0855ff49ebdc124
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 21:00:31 +0300

    Update BotApplication

--- link-tracker/bot/src/main/java/ru/asvronsky/bot/BotApplication.java
+++ link-tracker/bot/src/main/java/ru/asvronsky/bot/BotApplication.java
@@ -1,9 +1,15 @@
 package ru.asvronsky.bot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import ru.asvronsky.bot.configuration.ApplicationConfig;
 
+@SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class BotApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(BotApplication.class, args);
