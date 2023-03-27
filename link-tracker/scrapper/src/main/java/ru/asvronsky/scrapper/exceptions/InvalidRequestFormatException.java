acf934ed6c2122d6b54a14a2bd8911c6a2da9959
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 21:16:09 +0300

    Update InvalidRequestFormatException

--- link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/exceptions/InvalidRequestFormatException.java
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/exceptions/InvalidRequestFormatException.java
@@ -2,20 +2,8 @@ package ru.asvronsky.scrapper.exceptions;
 
 public class InvalidRequestFormatException extends RestResponseException{
 
-    public InvalidRequestFormatException(String message, String code, Exception cause) {
-        super(message, "Invalid request format", code, cause);
-    }
-
-    public InvalidRequestFormatException(String message, String code) {
-        super(message, "Invalid request format", code, null);
-    }
-    
-    public InvalidRequestFormatException(Exception cause) {
-        this("Invalid request format", "400", cause);
-    }
-
-    public InvalidRequestFormatException() {
-        this("Invalid request format", "400", null);
+    public InvalidRequestFormatException(String message) {
+        super(message);
     }
 
 }
