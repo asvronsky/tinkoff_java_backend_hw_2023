c257d12d4167b7a820af967430e64acf18356557
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 22:08:29 +0300

    Add swagger annotation

--- link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
+++ link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
@@ -4,8 +4,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import ru.asvronsky.bot.dto.UpdateLinksResponse;
+import ru.asvronsky.bot.dto.ApiErrorResponse;
+import ru.asvronsky.bot.dto.UpdateLinksRequest;
 import ru.asvronsky.bot.services.BotService;
 
 @RestController
@@ -14,9 +20,16 @@ import ru.asvronsky.bot.services.BotService;
 public class BotController {
     private final BotService botService;
     
-    @PostMapping
-    public UpdateLinksResponse updateLinks() {
-        return botService.updateLinks();
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Updated"),
+        @ApiResponse(responseCode = "400", description = "Invalid request format",
+            content = @Content(
+                    schema = @Schema(implementation = ApiErrorResponse.class),
+                    mediaType = "application/json"))
+    })
+    @PostMapping(consumes = "application/json")
+    public void updateLinks(@RequestBody(required = true) UpdateLinksRequest request) {
+        
     }
 
 }
