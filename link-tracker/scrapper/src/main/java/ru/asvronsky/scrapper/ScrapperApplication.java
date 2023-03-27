ddec499058c18e231001a1c995553aabb995adf4
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 23:06:10 +0300

    Add scrapper api title

--- link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/ScrapperApplication.java
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/ScrapperApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import ru.asvronsky.scrapper.configuration.ApplicationConfig;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
+@OpenAPIDefinition(info = @Info(title = "Swagger API", version = "1.0.0"))
 public class ScrapperApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
