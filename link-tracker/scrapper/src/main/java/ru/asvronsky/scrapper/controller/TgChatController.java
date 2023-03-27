ed36d379614e35264aa79a0c476a79d51d064f19
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 23:06:19 +0300

    Update docs

--- link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/controller/TgChatController.java
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/controller/TgChatController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import ru.asvronsky.scrapper.dto.ApiErrorResponse;
 import ru.asvronsky.scrapper.services.TgChatService;
 
 @RestController
@@ -15,11 +21,32 @@ import ru.asvronsky.scrapper.services.TgChatService;
 public class TgChatController {
     private final TgChatService tgChatService;
     
+    @Operation(summary = "Add chat")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Chat registered"),
+        @ApiResponse(responseCode = "400", description = "Invalid request format",
+            content = @Content(
+                    schema = @Schema(implementation = ApiErrorResponse.class),
+                    mediaType = "application/json"))
+    })
     @PostMapping("/{id}")
     public void registerChat(@PathVariable long id) {
         tgChatService.registerChat(id);
     }
 
+
+    @Operation(summary = "Delete chat")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Chat deleted"),
+        @ApiResponse(responseCode = "400", description = "Invalid request format",
+            content = @Content(
+                    schema = @Schema(implementation = ApiErrorResponse.class),
+                    mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "Chat not found",
+            content = @Content(
+                    schema = @Schema(implementation = ApiErrorResponse.class),
+                    mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public void deleteChat(@PathVariable long id) {
         tgChatService.deleteChat(id);
