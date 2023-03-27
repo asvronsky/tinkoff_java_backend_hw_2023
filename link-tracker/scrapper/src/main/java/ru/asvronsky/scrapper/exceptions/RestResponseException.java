50612ffc539df493ea645ccc215b9c348f02fa3f
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 21:15:52 +0300

    Remove fields from RestResponseException

--- link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/exceptions/RestResponseException.java
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/exceptions/RestResponseException.java
@@ -1,19 +1,8 @@
 package ru.asvronsky.scrapper.exceptions;
 
-import lombok.Getter;
-
-@Getter
 public abstract class RestResponseException extends RuntimeException {
-    private final String description;
-    private final String code;
     
-    public RestResponseException(String message, String description, String code, Exception cause) {
-        super(message, cause);
-        this.description = description;
-        this.code = code;
-    }
-
-    public RestResponseException(String message, String description, String code) {
-        this(message, description, code, null);
+    public RestResponseException(String message) {
+        super(message);
     }
 }
