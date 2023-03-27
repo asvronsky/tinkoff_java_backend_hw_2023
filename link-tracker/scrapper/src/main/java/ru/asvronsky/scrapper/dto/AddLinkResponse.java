36e7eba0e635b8a811029b320d6ec11bcd7d230f
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 19:37:13 +0300

    Add Scrapper dto

---
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/dto/AddLinkResponse.java
@@ -0,0 +1,5 @@
+package ru.asvronsky.scrapper.dto;
+
+public record AddLinkResponse(long id, String url) {
+    
+}
---
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/dto/DeleteLinkResponse.java
@@ -0,0 +1,5 @@
+package ru.asvronsky.scrapper.dto;
+
+public record DeleteLinkResponse(long id, String url) {
+    
+}
