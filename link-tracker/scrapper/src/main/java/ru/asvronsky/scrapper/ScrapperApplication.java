05b86235fb083d431ba1fcf15c110c47202d13c6
Alexander Vronsky <vronskiy2004@gmail.com>
2023-03-27 19:34:51 +0300

    Update ScrapperApplication annotations

--- link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/ScrapperApplication.java
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/ScrapperApplication.java
@@ -1,9 +1,15 @@
 package ru.asvronsky.scrapper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import ru.asvronsky.scrapper.configuration.ApplicationConfig;
 
+@SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class ScrapperApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
