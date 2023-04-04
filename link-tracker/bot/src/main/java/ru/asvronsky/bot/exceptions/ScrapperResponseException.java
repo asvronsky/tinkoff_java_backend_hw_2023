27818d83248731f79c08fbe23264275ad8816469
Alexander Vronsky <vronskiy2004@gmail.com>
2023-04-04 19:19:40 +0300

    Update exception handling

---
+++ link-tracker/bot/src/main/java/ru/asvronsky/bot/exceptions/ScrapperResponseException.java
@@ -0,0 +1,9 @@
+package ru.asvronsky.bot.exceptions;
+
+public class ScrapperResponseException extends RuntimeException {
+
+    public ScrapperResponseException(String message) {
+        super(message);
+    }
+    
+}
