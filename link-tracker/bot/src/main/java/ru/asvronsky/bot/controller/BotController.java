c957c3ceb25e6505afae1714de1cc767317ede97
Alexander Vronsky <vronskiy2004@gmail.com>
2023-04-18 19:31:21 +0300

    Update bot service

--- link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
+++ link-tracker/bot/src/main/java/ru/asvronsky/bot/controller/BotController.java
@@ -1,37 +1,20 @@
 package ru.asvronsky.bot.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import ru.asvronsky.shared.shareddto.ApiErrorResponse;
-import ru.asvronsky.shared.botdto.UpdateLinksRequest;
 import ru.asvronsky.bot.services.BotService;
+import ru.asvronsky.shared.botdto.UpdateLinksRequest;
 
 @RestController
-@RequestMapping("/updates")
 @RequiredArgsConstructor
-public class BotController {
+public class BotController implements BotControllerApi {
+
     private final BotService botService;
     
-    @Operation(summary = "Request update")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Updated"),
-        @ApiResponse(responseCode = "400", description = "Invalid request format",
-            content = @Content(
-                    schema = @Schema(implementation = ApiErrorResponse.class),
-                    mediaType = "application/json"))
-    })
-    @PostMapping(consumes = "application/json")
-    public void updateLinks(@RequestBody(required = true) UpdateLinksRequest request) {
-        botService.updateLinks();
+    @Override
+    public void updateLinks(UpdateLinksRequest request) {
+        botService.sendUpdate(request.description(), request.chatIds());
     }
 
 }
