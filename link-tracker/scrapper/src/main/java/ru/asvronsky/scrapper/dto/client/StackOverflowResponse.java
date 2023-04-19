becf3695e759ca6ed50c3f29bd07feec0283efdd
Alexander Vronsky <vronskiy2004@gmail.com>
2023-04-19 23:34:36 +0300

    Update tests

--- link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/dto/client/StackOverflowResponse.java
+++ link-tracker/scrapper/src/main/java/ru/asvronsky/scrapper/dto/client/StackOverflowResponse.java
@@ -5,7 +5,9 @@ import java.time.OffsetDateTime;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record StackOverflowResponse(
-    @JsonProperty("creation_date") OffsetDateTime creationDate
+    @JsonProperty("creation_date") OffsetDateTime creationDate,
+    @JsonProperty("answer_count") Integer answerCount,
+    @JsonProperty("score") Integer score
 ) {
     
 }
--- link-tracker/scrapper/src/test/java/ru/asvronsky/scrapper/clients/StackOverflowClientTest.java
+++ link-tracker/scrapper/src/test/java/ru/asvronsky/scrapper/clients/StackOverflowClientTest.java
@@ -6,27 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import ru.asvronsky.scrapper.dto.client.ListStackOverflowResponse;
 import ru.asvronsky.scrapper.dto.client.StackOverflowResponse;
 
 @Disabled
 public class StackOverflowClientTest {
     @Test
     public void basicTest() {
-        StackOverflowClient stackOverflowClient = StackOverflowClient.create();
-        ListStackOverflowResponse response = stackOverflowClient.getStackOverflowData(1642028);
+        StackOverflowClient stackOverflowClient = StackOverflowClient.create("");
+        StackOverflowResponse response = stackOverflowClient.getStackOverflowData(1642028);
         assertNotNull(response);
-        assertNotNull(response.items());
-        assertEquals(1, response.items().size());
-        List<StackOverflowResponse> items = response.items();
         assertEquals(OffsetDateTime.of(
                         LocalDateTime.ofEpochSecond(1256799465, 0, ZoneOffset.UTC), 
                         ZoneOffset.UTC), 
-                    items.get(0).creationDate());
+                    response.creationDate());
     }
 }
